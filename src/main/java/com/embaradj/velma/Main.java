4dbf60bcac92e91225fbe8be34fb86ae5e848f77
Emil Bäckstrand <emba2001@student.miun.se>
2023-03-07 20:52:28 +0100

    fixed bug related to folder structure, implemented functionality to create needed directories on runtime if needed

--- src/main/java/com/embaradj/velma/Main.java
+++ src/main/java/com/embaradj/velma/Main.java
@@ -6,7 +6,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
+    static DirCheckCreate dirCheck = DirCheckCreate.INSTANCE;
     public static void main(String[] args) {
+        // Create needed folders before program starts
+        dirCheck.checkDir("resources/");
+        dirCheck.checkDir("resources/pdfs");
+        dirCheck.checkDir("resources/processeddata");
+        dirCheck.checkDir("resources/rawdata/");
+        dirCheck.checkDir("resources/rawdata/hve");
+        dirCheck.checkDir("resources/rawdata/job");
+
         EventQueue.invokeLater(() -> {
             DataModel model = new DataModel();
             Controller controller = new Controller(model);
