ed38be01c92f8b849ab5d3d05fb693e69ec2c8d2
Rasmus Djupedal <radj2000@student.miun.se>
2023-03-08 15:14:03 +0100

    Settings class implemented ssyk codes

--- src/main/java/com/embaradj/velma/Settings.java
+++ src/main/java/com/embaradj/velma/Settings.java
@@ -1,10 +1,26 @@
 package com.embaradj.velma;
 
+import java.util.ArrayList;
+
+/**
+ * Eager init Singleton to keep settings and global variables
+ */
 public class Settings {
 
-    public boolean debug = true;
-
     private static Settings settings = new Settings();
+    public boolean DEBUG = true;
+    private ArrayList<Ssyk> ssyk;
+
+    private Settings() {
+        ssyk.add(new Ssyk("UXKZ_3zZ_ipB", "des1", true));
+        ssyk.add(new Ssyk("DJh5_yyF_hEM", "des1", true));
+        ssyk.add(new Ssyk("Q5DF_juj_8do", "des1", true));
+        ssyk.add(new Ssyk("D9SL_mtn_vGM", "des1", true));
+        ssyk.add(new Ssyk("cBBa_ngH_fCx", "des1", true));
+        ssyk.add(new Ssyk("BAeH_eg8_T2d", "des1", true));
+        ssyk.add(new Ssyk("UxT1_tPF_Kbg", "des1", true));
+    }
+
 
     public static Settings getInstance() { return settings; }
 
---
+++ src/main/java/com/embaradj/velma/Ssyk.java
@@ -0,0 +1,13 @@
+package com.embaradj.velma;
+
+public class Ssyk {
+
+    private String code, description;
+    private boolean selected;
+
+    public Ssyk(String code, String description, boolean selected) {
+        this.code = code;
+        this.description = description;
+        this.selected = selected;
+    }
+}
